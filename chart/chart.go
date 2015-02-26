6804e4405a81df9b6620e8a95d48903310953c95
ikurakin <icur@ccstele.co.uk>
2015-02-26 12:34:34 +0200

    added writing image binary data to string

--- chart/chart.go
+++ chart/chart.go
@@ -1,11 +1,17 @@
 package chart
 
 import (
+	"bufio"
+	"bytes"
 	"code.google.com/p/plotinum/plot"
 	"code.google.com/p/plotinum/plotter"
 	"code.google.com/p/plotinum/vg"
+	"code.google.com/p/plotinum/vg/vgsvg"
+	"encoding/base64"
+	"fmt"
 	"github.com/datastream/holtwinters"
 	"image/color"
+	"math"
 )
 
 type CustomChart struct {
@@ -21,6 +27,7 @@ func New(title, xtext, ytext string) *CustomChart {
 	}
 	p.Title.Text = title
 	p.X.Label.Text = xtext
+	p.X.Tick.Marker = DefaultTicks
 	p.Y.Label.Text = ytext
 	p.Add(plotter.NewGrid())
 	p.Legend.Top = true
@@ -30,15 +37,16 @@ func New(title, xtext, ytext string) *CustomChart {
 		Plot:     p,
 		LineDone: c,
 		LineColors: map[string]color.Color{
-			"green": color.RGBA{G: 255, A: 255},
-			"blue":  color.RGBA{B: 255, A: 255},
-			"red":   color.RGBA{R: 255, A: 255},
-			"gray":  color.RGBA{R: 180, G: 180, B: 180, A: 255},
+			"green":  color.RGBA{G: 255, A: 255},
+			"blue":   color.RGBA{B: 255, A: 255},
+			"red":    color.RGBA{R: 255, A: 255},
+			"gray":   color.RGBA{R: 180, G: 180, B: 180, A: 255},
+			"yellow": color.RGBA{R: 255, G: 220, A: 255},
 		},
 	}
 }
 
-func (cc *CustomChart) CreateLine(pval []float64, color string) *plotter.Line {
+func (cc *CustomChart) createLine(pval []float64, color string) *plotter.Line {
 	lineData := xysPoints(pval)
 	l, err := plotter.NewLine(lineData)
 	if err != nil {
@@ -50,7 +58,7 @@ func (cc *CustomChart) CreateLine(pval []float64, color string) *plotter.Line {
 	return l
 }
 
-func (cc *CustomChart) AddLine(legend string, l *plotter.Line) {
+func (cc *CustomChart) addLine(legend string, l *plotter.Line) {
 	cc.Plot.Add(l)
 	cc.Plot.Legend.Add(legend, l)
 	cc.LineDone <- "Done"
@@ -58,26 +66,43 @@ func (cc *CustomChart) AddLine(legend string, l *plotter.Line) {
 
 func (cc *CustomChart) CreatePreviousDayLine(pval []float64, color string) {
 	go func() {
-		l := cc.CreateLine(pval, color)
+		l := cc.createLine(pval, color)
 		c := cc.LineColors[color]
 		l.ShadeColor = &c
-		cc.AddLine("line", l)
+		cc.addLine("prev", l)
 	}()
 }
 
 func (cc *CustomChart) CreateCurrentDayLine(pval []float64, color string) {
 	go func() {
-		l := cc.CreateLine(pval, color)
-		cc.AddLine("line", l)
+		l := cc.createLine(pval, color)
+		cc.addLine("curent", l)
 	}()
 }
 
 func (cc *CustomChart) CreatePredictLine(pval []float64, color string) {
 	go func() {
 		prediction, _ := holtwinters.Forecast(pval, 0.1, 0.0035, 0.1, 4, 4)
-		l := cc.CreateLine(prediction, color)
-		cc.AddLine("predict", l)
+		l := cc.createLine(prediction, color)
+		cc.addLine("predict", l)
 	}()
+}
+
+func (cc *CustomChart) GetRawDataImg(w, h float64) (imgData string) {
+	c := vgsvg.New(vg.Points(w), vg.Points(h))
+	// Draw to the Canvas.
+	da := plot.MakeDrawArea(c)
+	cc.Plot.Draw(da)
+
+	// Write the Canvas to a io.Writer.
+	var b bytes.Buffer
+	buf := bufio.NewWriter(&b)
+	defer buf.Flush()
+	if _, err := c.WriteTo(buf); err != nil {
+		panic(err)
+	}
+	imgData = base64.StdEncoding.EncodeToString(b.Bytes())
+	return
 }
 
 func xysPoints(p []float64) plotter.XYs {
@@ -87,4 +112,62 @@ func xysPoints(p []float64) plotter.XYs {
 		pts[i].Y = p[i]
 	}
 	return pts
+}
+
+func DefaultTicks(min, max float64) (ticks []plot.Tick) {
+	const SuggestedTicks = 3
+	if max < min {
+		panic("illegal range")
+	}
+	tens := math.Pow10(int(math.Floor(math.Log10(max - min))))
+	n := (max - min) / tens
+	for n < SuggestedTicks {
+		tens /= 10
+		n = (max - min) / tens
+	}
+
+	majorMult := int(n / SuggestedTicks)
+	switch majorMult {
+	case 7:
+		majorMult = 6
+	case 9:
+		majorMult = 8
+	}
+	majorDelta := float64(majorMult) * tens
+	val := math.Floor(min/majorDelta) * majorDelta
+	for val <= max {
+		if val >= min && val <= max {
+			ticks = append(ticks, plot.Tick{Value: val, Label: fmt.Sprintf("%g", float32(val))})
+		}
+		if math.Nextafter(val, val+majorDelta) == val {
+			break
+		}
+		val += majorDelta
+	}
+
+	minorDelta := majorDelta / 2
+	switch majorMult {
+	case 3, 6:
+		minorDelta = majorDelta / 3
+	case 5:
+		minorDelta = majorDelta / 5
+	}
+
+	val = math.Floor(min/minorDelta) * minorDelta
+	for val <= max {
+		found := false
+		for _, t := range ticks {
+			if t.Value == val {
+				found = true
+			}
+		}
+		if val >= min && val <= max && !found {
+			ticks = append(ticks, plot.Tick{Value: val})
+		}
+		if math.Nextafter(val, val+minorDelta) == val {
+			break
+		}
+		val += minorDelta
+	}
+	return
 }
